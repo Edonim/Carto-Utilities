3b191e4175cd2a71175ce24d2b4d3396b44e357f
Edoardo Guerra <147606665+Edonim@users.noreply.github.com>
2025-02-18 17:03:05 +0100

    Versione iniziale
    Versione iniziale dello script, cerca il primo nodo write nella scena di Harmony e imposta ProRes 4444 in automatico e imposta il folder corretto, per ora è hardcoded per il progetto principale, vedrò di modificare poi

---
+++ Set to ProRes.js
@@ -0,0 +1,34 @@
+﻿function ProRes(exportFolder) {
+    var exportPath = "\\\\srv-harmony24\\usadata000\\00_OUTPUTMOV\\MORTINA\\" + exportFolder + "\\"
+        System.println("percorso impostato:" + exportPath);
+    var Type = ["WRITE", "null"];
+    var burninnode = "Top/Burn-In";
+    var sceneName = scene.currentScene();
+    var Found = node.getNodes(Type);
+
+    for (var i = 0; i < Found.length; ++i) {
+        var path = Found[i];
+        if (node.type(path) == "null") {
+            MessageLog.trace("Write node: " + node.getName(path));
+        }
+        else if (node.type(path) == "WRITE") {
+            MessageLog.trace("Write: " + node.getName(path));
+        }
+    }
+
+
+    // Disattiva il nodo Burn-in, se attivo
+    if (node.getEnable(burninnode)) {
+        System.println("Burn-in attivo, lo disattivo...");
+        node.setEnable(burninnode, false);
+    }
+
+    if (exportFolder) {
+        node.setTextAttr(path, "MOVIE_PATH", 1, exportPath + sceneName);
+            System.println("Percorso di esportazione aggiornato a: " + exportPath + sceneName);
+        node.setTextAttr(path, "MOVIE_FORMAT", 1, "com.toonboom.prores.mov.1.0");
+        node.setTextAttr(path, "MOVIE_VIDEOAUDIO", 1, "com.toonboom.prores.mov.1.0:enableSound(1)com.toonboom.prores.mov.1.0:sampleRate(22050)com.toonboom.prores.mov.1.0:nChannels(2)com.toonboom.prores.mov.1.0:videoCodec(prores4444)com.toonboom.prores.mov.1.0:alpha(0)");
+        node.setTextAttr(path, "exportToMovie", 1, "Output Movie"); 
+
+    }
+}
